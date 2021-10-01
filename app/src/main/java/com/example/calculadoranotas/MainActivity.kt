22f1c0bca7c458eab070b3c34aa2c7a83517b3f7
Murillo <tumusx1@gmail.com>
2021-09-30 22:44:48 -0300

    Created app and first commit push

---
+++ app/src/main/java/com/example/calculadoranotas/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.calculadoranotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btCalcular = calcular
+        val resultado =  resultado
+
+        btCalcular.setOnClickListener {
+
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val media = (nota1 + nota2)/2
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+            if(media>=6 &&faltas<=10){
+                resultado.setText("Parabéns, você foi aprovado" + "\n" + "Nota final:" +media+ "\n" + "Faltas: " + faltas)
+            }
+            else{
+                resultado.setText("Infelizmente você foi reprovado" + "\n" + "Nota final:" +media+ "\n" + "Faltas: " + faltas)
+
+            }
+
+        }
+
+    }
+}
