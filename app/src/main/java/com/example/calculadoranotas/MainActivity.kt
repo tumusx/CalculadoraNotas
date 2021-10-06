79c5f7b517b1a830309f977417d5d304f53fb943
Murillo <tumusx1@gmail.com>
2021-10-06 18:37:18 -0300

    altered for learning

--- app/src/main/java/com/example/calculadoranotas/MainActivity.kt
+++ app/src/main/java/com/example/calculadoranotas/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.calculadoranotas
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.icu.number.IntegerWidth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var clicks= 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btCalcular = calcular
-        val resultado =  resultado
+        val resultado = resultado
+
+
 
         btCalcular.setOnClickListener {
 
@@ -18,15 +24,20 @@ class MainActivity : AppCompatActivity() {
             val media = (nota1 + nota2)/2
             val faltas = Integer.parseInt(faltas.text.toString())
 
+
             if(media>=6 &&faltas<=10){
                 resultado.setText("Parabéns, você foi aprovado" + "\n" + "Nota final: "  +media+ "\n" + "Faltas: " + faltas)
+                resultado.setTextColor(Color.GREEN)
             }
             else{
                 resultado.setText("Infelizmente você foi reprovado" + "\n" + "Nota final: "  +media+ "\n" + "Faltas: " + faltas)
+                resultado.setTextColor(Color.RED)
 
             }
 
         }
 
     }
-}
+
+
+    }
